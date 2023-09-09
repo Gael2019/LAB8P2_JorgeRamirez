5f1bbda778bb3c7a7450d1a753e1be60bbf6e0c6
Gael2019 <ramiritokillu@gmail.com>
2023-09-08 18:24:17 -0600

    clases

--- Lab8P2_JorgeRamirez/src/lab8p2_jorgeramirez/Nadador2.java
+++ Lab8P2_JorgeRamirez/src/lab8p2_jorgeramirez/Nadador2.java
@@ -4,10 +4,30 @@
  */
 package lab8p2_jorgeramirez;
 
+import javax.swing.JProgressBar;
+
 /**
  *
  * @author Jorge Ramirez
  */
-public class Nadador2 {
+public class Nadador2 extends Thread {
+    int distance;
+    JProgressBar barra;
+    int random;
+
+    public Nadador2(int distance, JProgressBar barra) {
+        this.distance = distance;
+        this.barra = barra;
+    }
     
+   @Override
+   public void run (){
+       while (barra.getValue() < distance){
+           barra.setValue(barra.getValue()+random);
+           try {
+               Thread.sleep(0);
+           } catch (Exception e) {
+           }
+       }
+   }
 }
--- Lab8P2_JorgeRamirez/src/lab8p2_jorgeramirez/Nadador3.java
+++ Lab8P2_JorgeRamirez/src/lab8p2_jorgeramirez/Nadador3.java
@@ -4,10 +4,30 @@
  */
 package lab8p2_jorgeramirez;
 
+import javax.swing.JProgressBar;
+
 /**
  *
  * @author Jorge Ramirez
  */
-public class Nadador3 {
+public class Nadador3 extends Thread{
+    int distance;
+    JProgressBar barra;
+    int random;
+
+    public Nadador3(int distance, JProgressBar barra) {
+        this.distance = distance;
+        this.barra = barra;
+    }
     
+   @Override
+   public void run (){
+       while (barra.getValue() < distance){
+           barra.setValue(barra.getValue()+random);
+           try {
+               Thread.sleep(0);
+           } catch (Exception e) {
+           }
+       }
+   }
 }
